1c1b4401e62735437df34a8a5c1152ece39184a0
LCJ-MinYa <1049468118@qq.com>
2018-02-01 10:11:28 +0800

    完美解决同步执行

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import config from './config/config';
+import mongoose from 'mongoose';
 import {
 	getHttpReq
 } from './utils/httpRequest';
 import Shy from './model/shy.server.module';
 
 async function requestPageNum() {
-	for (let i = 1; i < config.pageNum; i++) {
+	for (let i = 1; i <= config.pageNum; i++) {
 		await requestPageLine(i);
 	}
+	mongoose.disconnect();
 }
 
 function requestPageLine(pageNum) {
@@ -15,7 +17,7 @@ function requestPageLine(pageNum) {
 		let pathArray = [];
 		let url = config.url + config.path;
 		if (pageNum !== 1) {
-			url += '/p_' + i + '.html';
+			url += '/p_' + pageNum + '.html';
 		}
 		getHttpReq(url)
 			.then(($) => {
@@ -26,8 +28,15 @@ function requestPageLine(pageNum) {
 					}
 				})
 				console.log('第' + pageNum + '页目录请求成功');
-				requestDetailPage(pathArray, pageNum);
-				resolve(true);
+				requestDetailPage(pathArray, pageNum)
+					.then((data) => {
+						console.log('第' + pageNum + '页目录和详情保存完成');
+						resolve(data);
+					})
+					.catch((err) => {
+						console.log('第' + pageNum + '页目录和详情保存失败');
+						reject(err);
+					})
 			})
 			.catch((err) => {
 				console.log('请求第' + pageNum + '页目录失败');
@@ -37,9 +46,15 @@ function requestPageLine(pageNum) {
 }
 
 async function requestDetailPage(pathArray, pageNum) {
+	let lastPromise;
 	for (let i = 0; i < pathArray.length; i++) {
-		await saveShyData(pathArray[i], i, pageNum);
+		if (i == pathArray.length - 1) {
+			lastPromise = await saveShyData(pathArray[i], i, pageNum);
+		} else {
+			await saveShyData(pathArray[i], i, pageNum);
+		}
 	}
+	return lastPromise;
 }
 
 function saveShyData(path, i, pageNum) {
@@ -68,7 +83,7 @@ function saveShyData(path, i, pageNum) {
 					})
 			})
 			.catch((err) => {
-				console.log('请求第' + pageNum + '页第' + i + '条详情失败');
+				console.log('请求第' + pageNum + '页第' + (i + 1) + '条详情失败');
 				reject(err);
 			})
 	})
@@ -79,10 +94,10 @@ function shyInsert(content, i, pageNum) {
 		let shyData = new Shy(content);
 		shyData.save((err) => {
 			if (err) {
-				console.log('存储第' + pageNum + '页第' + i + '条数据失败');
+				console.log('存储第' + pageNum + '页第' + (i + 1) + '条数据失败');
 				reject(err);
 			} else {
-				console.log('存储第' + pageNum + '页第' + i + '条数据成功');
+				console.log('存储第' + pageNum + '页第' + (i + 1) + '条数据成功');
 				resolve(true);
 			}
 		})
